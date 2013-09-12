b42a4e3808b4ba4c1b05c139051e94b954239514
U-91waijiao-PC\91waijiao <91waijiao@91waijiao-PC.(none)>
2013-09-12 13:35:25 +0800

    init commit

---
+++ init.el
@@ -0,0 +1,13 @@
+;close wellcome msg
+(setq inhibit-startup-message t)
+;yasnippet
+(add-to-list 'load-path
+	     "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+;snippets
+(setq yas-snippet-dirs '("~/.emacs.d/snippets_kk"))
+(yas-global-mode 1)
+
+;color theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'tango-dark t)
