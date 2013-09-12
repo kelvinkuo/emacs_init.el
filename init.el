9427a792dec1cc74b2c424ca7b732110f4f9ca49
U-91waijiao-PC\91waijiao <91waijiao@91waijiao-PC.(none)>
2013-09-12 13:45:37 +0800

    show line num

--- init.el
+++ init.el
@@ -1,13 +1,20 @@
-;close wellcome msg
+;;kk
+
+;;close wellcome msg
 (setq inhibit-startup-message t)
-;yasnippet
+
+;;show line num
+(global-linum-mode t)
+
+;;yasnippet
 (add-to-list 'load-path
 	     "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
-;snippets
+
+;;snippets
 (setq yas-snippet-dirs '("~/.emacs.d/snippets_kk"))
 (yas-global-mode 1)
 
-;color theme
+;;color theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'tango-dark t)
