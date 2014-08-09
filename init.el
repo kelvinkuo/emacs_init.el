1eae06917b5ead803584c3e6b40c21a70a85c9e6
kelvinkuo <kelvinkuo224@gmail.com>
2014-08-09 10:06:41 +0800

    add auto-complete

--- init.el
+++ init.el
@@ -35,6 +35,27 @@
 (require 'ecb)
 (require 'ecb-autoloads)
 
+;; flymake
+(require 'flymake)
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
+;; yasnippet
+;;; yasnippet must be loaded before auto-complete, so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+
+
 ;;yasnippet
 ;;(add-to-list 'load-path
 ;;	     "~/.emacs.d/plugins/yasnippet")
@@ -65,3 +86,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
